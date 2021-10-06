136433a0d078af3bc5ca64116d654e87738f0407
Artem Lunin <luninartyom@yandex.ru>
2021-10-06 10:21:59 +0300

    Added lombok

--- src/main/java/com/sberbank/firstmodule/entities/CityEntity.java
+++ src/main/java/com/sberbank/firstmodule/entities/CityEntity.java
@@ -1,26 +1,21 @@
 package com.sberbank.firstmodule.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class CityEntity {
-
-    public CityEntity() {
-    }
-
-    public CityEntity(Long id, String name,
-                      String region, String district,
-                      int population, int foundation) {
-        this.id = id;
-        this.name = name;
-        this.region = region;
-        this.district = district;
-        this.population = population;
-        this.foundation = foundation;
-    }
 
     @Id
     @GeneratedValue
@@ -31,54 +26,6 @@ public class CityEntity {
     private String district;
     private int population;
     private int foundation;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public int getPopulation() {
-        return population;
-    }
-
-    public void setPopulation(int population) {
-        this.population = population;
-    }
-
-    public int getFoundation() {
-        return foundation;
-    }
-
-    public void setFoundation(int foundation) {
-        this.foundation = foundation;
-    }
 
     @Override
     public String toString() {
